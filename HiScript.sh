e30ab28ae61d0339395d31cc6fb078d837fb9562
NikolayWTF <kolan.gregorin01@gmail.com>
2021-05-06 19:10:18 +0300

    Добавил абсолютную адресацию используя переменные окружения.

--- HiScript.sh
+++ HiScript.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
 echo "Добро пожаловать в дневник!"
+echo "Для начала работы укажите путь к Дневнику"
+echo "Например: Desktop/MyFavoriteDiary"
+read DIARY
+export DIARYFILE=$DIARY
+
 ./mainscript.sh
 
--- mainscript.sh
+++ mainscript.sh
@@ -3,7 +3,6 @@
 echo ""
 echo "Что вы хотите сделать?"
 echo "1 Сделать запись "
-#echo "2 Посмотреть статистику"
 echo "2 Открыть список всех записей"
 echo "3 Открыть корзину"
 echo "4 Закончить работу"
@@ -11,7 +10,7 @@ echo "Введите соответствующую цифру (1 2 3 4)"
 read comand
 
 if [ "$comand" == "1" ]; then
-	nano diary.txt
+	nano $HOME/$DIARYFILE/diary.txt
 	echo ""
 	echo ""
 	echo "Введите 'save' чтобы сохранить запись"
@@ -27,23 +26,21 @@ if [ "$comand" == "1" ]; then
 
 	if [ "$ans" == "save" ]; then
 
-		cat newsms.txt >> alldiary.txt
-		cat  diary.txt >>  alldiary.txt
-		rm diary.txt
+		cat $HOME/$DIARYFILE/newsms.txt >> $HOME/$DIARYFILE/alldiary.txt
+		cat $HOME/$DIARYFILE/diary.txt >> $HOME/$DIARYFILE/alldiary.txt
+		rm $HOME/$DIARYFILE/diary.txt
 		touch diary.txt
 		echo "Запись сохранена успешно!"
 		./mainscript.sh
 	elif [ "$ans" == "delete" ]; then
-		cat newsms.txt >> basket.txt
-		cat  diary.txt >> basket.txt
-		rm diary.txt
+		cat $HOME/$DIARYFILE/newsms.txt >> $HOME/$DIARYFILE/basket.txt
+                cat $HOME/$DIARYFILE/diary.txt >> $HOME/$DIARYFILE/basket.txt
+                rm $HOME/$DIARYFILE/diary.txt
 		touch diary.txt
 		echo "Запись удалена успешно!"
 		./mainscript.sh
 	fi
 
-#elif [ "$comand" == "2" ]; then
-#	nano stat.txt
 elif [ "$comand" == "2" ]; then
 	nano alldiary.txt
 	./mainscript.sh
