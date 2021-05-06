3e40b638dd482bc92f4fff4488e9ff4355a1551f
NikolayWTF <kolan.gregorin01@gmail.com>
2021-05-06 19:24:20 +0300

    Добавил переменную окружения

--- HiScript.sh
+++ HiScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Добро пожаловать в дневник!"
-echo "Для начала работы укажите путь к Дневнику"
+echo "Для начала работы укажите путь к Дневнику "
 echo "Например: Desktop/MyFavoriteDiary"
 read DIARY
 export DIARYFILE=$DIARY
